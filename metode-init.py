416ad30ddefe5d6f429f2986373a5dccd438d5c0
adityadenny <aditaydenny6@gmail.com>
2022-03-07 14:55:54 +0700

    inheritance learn

--- metode-init.py
+++ metode-init.py
@@ -9,11 +9,23 @@ class Player:
     def getSpeed(self):
         return self.speed
 
+    def getSkill(self):
+        return 'normal'
+
+    def setAge(self, age):
+        self.age = age
+        return self.age
+
 
 class ArgentinaPlayer(Player):
     def setAge(self, age):
         self.age = age
         return self.age
+
+
+class BrazilPlayer(Player):
+    def getSkill(self):
+        return 'samba'
 
 
 player = Player('Kai Havertz', '90')
@@ -23,3 +35,8 @@ player3 = ArgentinaPlayer('Messi', '98')
 print(player.getName() + " punya speed " + player.getSpeed())
 print(player2.getName() + " punya speed " + player2.getSpeed())
 print(player3.getName() + " umurnya " + player3.setAge('33'))
+
+print('\n')
+player4 = BrazilPlayer('Neymar', '97')
+print(player4.getName() + " skillnya " + player4.getSkill() + " speednya " + player4.getSpeed() + ' umurnya ' +
+      player4.setAge('29'))
